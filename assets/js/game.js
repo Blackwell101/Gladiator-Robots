1ef274fed8902281a86e66c727c7ebd069bad240
Blackwell101 <103456893+Blackwell101@users.noreply.github.com>
2022-06-18 17:34:49 -0400

    java robot fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,7 @@
-fight(); {
-    window.alert("The fight has begun!");
-}
+function fight() { 
+    var playerName = window.prompt("What is your robot's name?");
+    console.log(playerName);
+    console.log("This logs a string, good for leaving yourself a message");
+    console.log(10+10);
+    console.log("Our robot's name is " + playerName);
+} fight();
